ac30b694111e1d8c1abcbe8709987fe18359ea84
kismetzero <kismetzero@qq.com>
2026-09-17 16:03:23 +0800

    [更新] 更新BroforcePlugin，添加Broforce逆向信息

--- Bepinex/BroforcePlugin/PluginTest.cs
+++ Bepinex/BroforcePlugin/PluginTest.cs
@@ -1,87 +1,87 @@
-﻿using System;
-using BepInEx;
-using UnityEngine;
+﻿using BepInEx;
 using HarmonyLib;
+using System;
+using System.Reflection.Emit;
+using UnityEngine;
 
 namespace BroforcePlugin
 {
-    [BepInPlugin("com.kisme.BepInEx.Broforce.PluginTest", "MyFirstBroforceBepInExMod", "1.0")]
+    [BepInPlugin("com.kisme.BepInEx.Broforce.PluginTest", "BroforcePluginTest", "1.0")]
     public class PluginTest : BaseUnityPlugin
     {
         public static PluginTest _instance;
-        public bool UpdateFirstOn;
+        public bool UpdateFirstOn = true;
 
-        public static HeroController i_hc_awake;
-        public static HeroController i_hc_start;
+        public int pNum = 0;
+        public HeroType hType = HeroType.IndianaBrones;
 
         void Awake()
         {
-            Logger.LogInfo("Hello World！！！");
-            Logger.LogInfo("BroforcePluginTest: Awake()");
-            //初始化参数
+            Logger.LogInfo("Awake()");
             _instance = this;
-            Harmony.CreateAndPatchAll(typeof(PluginTest));
+            Harmony.CreateAndPatchAll(typeof(CheatFunc));
+        }
+        void OnDestroy()
+        {
+            Logger.LogInfo("OnDestroy()");
+        }
+        void OnEnable()
+        {
+            Logger.LogInfo("OnEnable()");
+        }
+        void OnDisable()
+        {
+            Logger.LogInfo("OnDisable()");
         }
 
         void Start()
         {
-            Logger.LogInfo("BroforcePluginTest: Start()");
+            Logger.LogInfo("Start()");
         }
 
         void Update()
         {
             if (this.UpdateFirstOn)
             {
-                Logger.LogInfo("BroforcePluginTest: First Update()");
+                Logger.LogInfo("First Update()");
                 this.UpdateFirstOn = false;
             }
 
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                //1：增加生命
-                HeroController.AddLife(0);
+                CheatFunc.a_addLife(pNum);
             }
-
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                //2：切换角色
-                HeroController.ChangeBro(0, HeroType.IndianaBrones);
+                CheatFunc.a_addAmmo(pNum);
             }
-
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                //3：增加特殊弹药
-                var broBase = HeroController.players[0].character as BroBase;
-                broBase.SpecialAmmo += 1;
+                CheatFunc.a_changeBro(pNum, hType);
             }
-        }
-
-        void OnGUI()
-        {
-            if (WindowsDisplayOn)
+            if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                Cursor.visible = true;
-                // 定义窗口位置 x y 宽 高
-                Rect windowRect = new Rect(100, 100, 200, 200);
-                /* 创建一个新窗口
-                       注意：第一个参数(114514)为窗口ID，ID尽量设置的与众不同，
-                       若与其他Mod的窗口ID相同，将会导致窗口冲突  */
-                windowRect = GUI.Window(114514, windowRect, DoMyWindow, "修改器窗口");
+                CheatFunc.a_godMode(pNum);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                CheatFunc.a_infAmmo();
             }
         }
 
-        //[HarmonyPrefix, HarmonyPatch(typeof(HeroController), "Awake")]
-        //public static void getHcAwake(HeroController __instance)
+        //void OnGUI()
         //{
-        //    i_hc_awake = __instance;
-        //    Debug.Log("BroforcePluginTest: getHcAwake()");
+        //    if (WindowsDisplayOn)
+        //    {
+        //        Cursor.visible = true;
+        //        // 定义窗口位置 x y 宽 高
+        //        Rect windowRect = new Rect(100, 100, 200, 200);
+        //        /* 创建一个新窗口
+        //               注意：第一个参数(114514)为窗口ID，ID尽量设置的与众不同，
+        //               若与其他Mod的窗口ID相同，将会导致窗口冲突  */
+        //        windowRect = GUI.Window(114514, windowRect, DoMyWindow, "修改器窗口");
+        //    }
         //}
 
-        //[HarmonyPrefix, HarmonyPatch(typeof(HeroController), "Start")]
-        //public static void getHcStart(HeroController __instance)
-        //{
-        //    i_hc_start = __instance;
-        //    Debug.Log("BroforcePluginTest: getHcStart()");
-        //}
     }
 }
---
+++ Reverse/Broforce.md
@@ -0,0 +1,40 @@
+## 武装原型 Broforce
+
+### ~.
+
+```csharp
+
+public class Unit : NetworkedUnit {
+    public virtual bool invulnerable;
+}
+
+public class TestVanDammeAnim : Unit { }
+
+public class BroBase : TestVanDammeAnim {
+    public override int SpecialAmmo;
+}
+
+public class Player : NetworkObject {
+    public TestVanDammeAnim character;
+    public void AddLife();
+    public void RemoveLife();
+    public void SpawnHero(HeroType nextHeroType);
+}
+
+public class HeroController : NetworkObject, ISerializationCallbackReceiver {
+    public static HeroController Instance;
+    public static Player[] players = new Player[4];
+    public static void AddLife(int playerNum);
+    public static void ChangeBro(int playerNum, HeroType newHeroType);
+}
+
+HeroController.players[playerNum].character.invulnerable = true;
+
+HeroController.AddLife(playerNum);
+
+HeroController.ChangeBro(playerNum, HeroType.None);
+
+var broBase = HeroController.players[playerNum].character as BroBase;
+broBase.SpecialAmmo += 1;
+
+```
