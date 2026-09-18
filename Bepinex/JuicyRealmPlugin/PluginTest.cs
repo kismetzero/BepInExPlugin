8b3f72f7f74de4b8407aee416ba66236fb9bd7ed
kismetzero <kismetzero@qq.com>
2026-09-18 08:42:46 +0800

    [添加] 添加多个项目

---
+++ Bepinex/JuicyRealmPlugin/PluginTest.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace JuicyRealmPlugin
+{
+    [BepInPlugin("com.kisme.BepInEx.JuicyRealm.PluginTest", "JuicyRealmPluginTest", "1.0")]
+    public class PluginTest : BaseUnityPlugin
+    {
+        public static PluginTest _instance;
+        public bool UpdateFirstOn = true;
+        void Awake()
+        {
+            Logger.LogInfo("Awake()");
+            _instance = this;
+        }
+        void OnDestroy()
+        {
+            Logger.LogInfo("OnDestroy()");
+        }
+        void OnEnable()
+        {
+            Logger.LogInfo("OnEnable()");
+        }
+        void OnDisable()
+        {
+            Logger.LogInfo("OnDisable()");
+        }
+
+        void Start()
+        {
+            Logger.LogInfo("Start()");
+        }
+
+        void Update()
+        {
+            if (this.UpdateFirstOn)
+            {
+                Logger.LogInfo("First Update()");
+                this.UpdateFirstOn = false;
+            }
+        }
+    }
+}
